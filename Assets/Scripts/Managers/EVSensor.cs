35bae154341c84ca9ede40775981336c1b2a2958
wchia015 <51470996+wchia015@users.noreply.github.com>
2020-04-04 13:50:17 +0800

    Testing Timer

--- Assets/Scripts/Managers/EVSensor.cs
+++ Assets/Scripts/Managers/EVSensor.cs
@@ -6,7 +6,10 @@ public class EVSensor : MonoBehaviour
 {
     private Dictionary<int, float> air_temperature;
     public float Temp { get;  set; }
-    [SerializeField] private int startTiming;
+    private List<int> SetOfTimings;
+    private int FirstTiming;
+    private int LastTiming;
+
 
     void Awake()
     {
@@ -20,6 +23,11 @@ public class EVSensor : MonoBehaviour
 
     void Update()
     {
+        while ( FirstTiming<=LastTiming)
+        {
+            FirstTiming += (int)Time.deltaTime;
+            Debug.Log("Timer for start time " + FirstTiming);
+        }
         /*Debug.Log("Startof Timer " + startTiming);       //Timer
         for (int jj = 0; jj < TempTimings.Count; jj++)
             if (startTiming == TempTimings[jj])
@@ -35,10 +43,41 @@ public class EVSensor : MonoBehaviour
         Debug.Log("received SensorTemp from OnSensorTempLoaded for Sensor: " + gameObject.name);
 
         air_temperature = Managers.Sensors.GetAirTemp(gameObject.name);
-        /*foreach (var items in air_temperature)
+        SetOfTimings = new List<int>(air_temperature.Keys);
+        if (SetOfTimings.Count != 0) // LP: to set boolean for int variables, set != 0 instead of null
+        {
+            FirstTiming = SetOfTimings[0];
+            LastTiming = SetOfTimings[SetOfTimings.Count - 1];
+        }
+
+        StartCoroutine(StartTimer());
+        /*Debug.Log(FirstTiming + " FirstTiming SensorName: " + gameObject.name);
+        Debug.Log(LastTiming + " LastTiming SensorName: " + gameObject.name);
+
+        foreach (var items in SetOfTimings)
+        {
+            Debug.Log(items + " SensorName: " + gameObject.name);
+        }
+
+        foreach (var items in air_temperature)
         {
             Debug.Log(items.Key + " " + items.Value + " SensorName: " + gameObject.name);
         }*/
+    }
+
+
+    private IEnumerator StartTimer()
+    {
+        /*while ( FirstTiming<=LastTiming)
+        {
+            FirstTiming += (int)Time.deltaTime;
+            Debug.Log("Timer for start time " + FirstTiming);
+            yield return null;
+        }*/
+
+        yield return null;
+
+        
     }
 
     void ChangeSensorTemp (int key)
